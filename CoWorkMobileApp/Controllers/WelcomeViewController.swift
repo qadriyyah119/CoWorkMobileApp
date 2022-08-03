21bd88adbd9f9512a3f3228d73e542b8a49e1457
qadriyyah119 <qmadinah@gmail.com>
2022-08-03 15:28:41 -0400

    configure programmatic setup

--- CoWorkMobileApp/Controllers/WelcomeViewController.swift
+++ CoWorkMobileApp/Controllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  CoWorkMobileApp
 //
 //  Created by Qadriyyah Thomas on 8/3/22.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
 
 
