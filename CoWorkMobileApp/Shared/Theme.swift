56ddecbbb591f149c116040af6233515d207e0cf
qadriyyah119 <qmadinah@gmail.com>
2022-08-03 15:09:34 -0400

    Add Theme file

---
+++ CoWorkMobileApp/Shared/Theme.swift
@@ -0,0 +1,22 @@
+//
+//  Theme.swift
+//  CoWorkMobileApp
+//
+//  Created by Qadriyyah Thomas on 8/3/22.
+//
+
+import UIKit
+
+struct ThemeFonts {
+    static let headerFont = "TimesNewRomanPS-BoldMT"
+    static let bodyFont = "Roboto-Regular"
+    static let buttonFont = "Roboto-Bold"
+    static let bodyFontMedium = "Roboto-Medium"
+    static let bodyFontThin = "Roboto-Thin"
+}
+
+struct ThemeColors {
+    static let grayColor = UIColor(named: "grayColor")
+    static let secondaryColor = UIColor(named: "secondaryColor")
+    static let mainBackgroundColor = UIColor(named: "mainBackgroundColor")
+}
