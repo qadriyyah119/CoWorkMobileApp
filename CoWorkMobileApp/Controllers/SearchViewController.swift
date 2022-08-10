db2b172705a587f6d9fb2ae7e491a167a15ad99b
qadriyyah119 <qmadinah@gmail.com>
2022-08-10 17:26:26 -0400

    Color option sample on searchViewController

--- CoWorkMobileApp/Controllers/SearchViewController.swift
+++ CoWorkMobileApp/Controllers/SearchViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    lazy var gradient: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        gradient.type = .axial
+        gradient.colors = [
+            ThemeColors.grayColor?.cgColor ?? UIColor.darkGray.cgColor,
+            UIColor.lightGray.cgColor,
+            UIColor.white.cgColor
+        ]
+        gradient.locations = [0, 0.25, 1]
+        return gradient
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = ThemeColors.grayColor
+        gradient.frame = view.bounds
+        view.layer.addSublayer(gradient)
+//        view.backgroundColor = ThemeColors.grayColor
         self.title = "Search"
     }
 }
