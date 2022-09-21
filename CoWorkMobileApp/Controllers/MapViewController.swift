73645f29b63ba3de9f9adc093811529d73a9dcf1
qadriyyah119 <qmadinah@gmail.com>
2022-09-21 17:17:52 -0400

    Add MapViewController WIP

---
+++ CoWorkMobileApp/Controllers/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  CoWorkMobileApp
+//
+//  Created by Qadriyyah Thomas on 9/21/22.
+//
+
+import UIKit
+import Cartography
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    private lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func setupView() {
+        self.view.addSubview(mapView)
+        
+        constrain(mapView) { mapView in
+            mapView.edges == mapView.superView!.edges
+        }
+    }
+    
+    
+}
